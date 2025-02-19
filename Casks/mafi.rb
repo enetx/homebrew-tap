5fe7f7dc635c0b58b6ddd7fbf1db128dac29fc5c
enetx <163449931+enetx@users.noreply.github.com>
2025-02-19 09:56:25 +0200

    v1.0.2 Default embedded font added.

--- Casks/mafi.rb
+++ Casks/mafi.rb
@@ -1,6 +1,6 @@
 cask "mafi" do
-  version "1.0.1"
-  sha256 "4ff4e4dd333da5179eea0b9453b09e2cb963a94d37c4855e8203f296892967cb"
+  version "1.0.2"
+  sha256 "63cdcaef0d6953542b239a9f910a6b76ab981189adbfbcb6e127e3dc9db76772"
 
   url "https://github.com/enetx/mafiapp/releases/download/v#{version}/Mafi.zip"
   name "Mafi"
