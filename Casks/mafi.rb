0853fae431dc6fa11ca45485441c88087bb82824
enetx <163449931+enetx@users.noreply.github.com>
2025-02-21 09:42:34 +0200

    v1.0.7 universal binary

--- Casks/mafi.rb
+++ Casks/mafi.rb
@@ -1,6 +1,6 @@
 cask "mafi" do
-  version "1.0.6"
-  sha256 "13ca94b0916de810fc87650733f39cad1bf1eb734e7c303c29a5a6e5912a548a"
+  version "1.0.7"
+  sha256 "a212f5a610c9ed52d12b5df3b9249279c4fb7b847bdf17992a1ce244553bfb40"
 
   url "https://github.com/enetx/mafiapp/releases/download/v#{version}/Mafi.zip"
   name "Mafi"
@@ -8,10 +8,24 @@ cask "mafi" do
   homepage "https://mafiapp.com"
 
   postflight do
-    system "xattr -d com.apple.quarantine #{appdir}/Mafi.app"
-    system "xattr -d com.apple.quarantine #{appdir}/Mafi.app/Contents/MacOS/mafi"
-    system "xattr -d com.apple.quarantine #{appdir}/Mafi.app/Contents/Helpers/cbagent"
+    system "xattr -dr com.apple.quarantine #{appdir}/Mafi.app"
+    system "codesign --force --deep --sign - #{appdir}/Mafi.app"
   end
 
   app "Mafi.app"
+
+  caveats <<~EOS
+    After installation, you may need to grant accessibility permissions to Mafi:
+    1. Open System Settings -> Privacy & Security -> Accessibility.
+    2. Click the "+" button and add "Mafi.app".
+    3. Restart "Mafi.app" for the changes to take effect.
+
+    If you encounter issues, try running:
+      xattr -rd com.apple.quarantine #{appdir}/Mafi.app
+      codesign --force --deep --sign - #{appdir}/Mafi.app
+
+    You can copy the default configuration file using:
+      mkdir -p ~/.config/mafi
+      cp /Applications/Mafi.app/Contents/Resources/default-config.toml ~/.config/mafi/config.toml
+  EOS
 end
